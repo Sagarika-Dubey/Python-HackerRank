0d6b57c7a570570763481fef24dfe86b132b0999
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-04-07 22:19:09 +0530

    set- mutations

---
+++ set_mutations.py
@@ -0,0 +1,10 @@
+a = int(input())
+A = set(map(int, input().split(" ")))
+N = int(input())
+
+for _ in range(N):
+    operation, _ = input().split(" ")
+    B = set(map(int, input().split(" ")))
+    exec(f"A.{operation}({B})")
+
+print(sum(A))
