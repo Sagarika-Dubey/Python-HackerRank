4fdf383d5c8cc6cc14204a7bb17821a492d84b2c
Sagarika-Dubey <sagarikardubey@gmail.com>
2024-06-04 12:33:58 +0530

    map and lambda functions

--- map_lambda.py
+++ map_lambda.py
@@ -15,8 +15,3 @@ def fibonacci(n):
 if __name__ == '__main__':
     n = int(input())
     print(list(map(cube, fibonacci(n))))
-
-
-if __name__ == '__main__':
-    n = int(input())
-    print(list(map(cube, fibonacci(n))))
